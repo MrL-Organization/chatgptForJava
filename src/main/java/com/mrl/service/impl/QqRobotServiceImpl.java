bbb2daf167fe519df9590e0498d13123517e27e4
MrL <204594564@qq.com>
2023-04-25 14:50:58 +0800

    机器人增加快速回复机制，响应更加及时

--- src/main/java/com/mrl/service/impl/QqRobotServiceImpl.java
+++ src/main/java/com/mrl/service/impl/QqRobotServiceImpl.java
@@ -18,6 +18,7 @@ import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Auther: MrL
@@ -50,9 +51,11 @@ public class QqRobotServiceImpl implements QqRobotService {
     //ChatGPTService chatGPTService;
 
     @Override
-    public void QqRobotEvenHandle(HttpServletRequest request) throws IOException {
+    public Map QqRobotEvenHandle(HttpServletRequest request) throws IOException {
         log.debug("QqRobotServiceImpl.QqRobotEvenHandle开始");
         JSONObject jsonParam = this.getJSONParam(request);
+        //快速回复的响应参数
+        HashMap<String,Object> result = new HashMap<>();
         String post_type = jsonParam.getString("post_type");
         String message = jsonParam.getString("message");
         String user_id = jsonParam.getString("user_id");
@@ -64,7 +67,7 @@ public class QqRobotServiceImpl implements QqRobotService {
                     if ("#开始聊天".equals(message)){
                         user_status.put(user_id,true);
                         user_time.put(user_id,new Date());
-                        aiMessage = "已开启连续聊天，会增加tokens的使用量，请不要过长连续聊天，30分钟未关闭连续聊天，则会自动关闭。";
+                        aiMessage = "已开启连续聊天，会增加tokens的使用量，聊天内容请不要过长，30分钟未关闭连续聊天，会自动关闭。";
                     }else if ("#结束聊天".equals(message)){
                         user_status.remove(user_id);
                         user_messages.remove(user_id);
@@ -85,16 +88,17 @@ public class QqRobotServiceImpl implements QqRobotService {
                         aiMessage = "未知命令，请输入【帮助】查看所有命令！";
                     }
                 } else if ("帮助".equals(message)) {
-                    aiMessage = "1.如果只想让AI回答问题，请直接输入问题；\n" +
-                            "2.如果要生成图片请说：【#生成图片 描述】；\n" +
-                            "3.输入【#查询余额】可以查询当前key的剩余美元；\n" +
-                            "4.输入【#开始聊天】，即可开始连续聊天，输入【#结束聊天】，即可结束聊天；\n" +
+                    aiMessage = "1.单问单答：如果只想让AI回答问题，请直接输入问题，例如【马化腾是谁】；\n" +
+                            "2.生成图片：如果要生成图片请说：【#生成图片 描述】，例如【#生成图片 打篮球的鸡】；\n" +
+                            "3.查询余额：输入【#查询余额】可以查询当前key的剩余美元；\n" +
+                            "4.连续聊天：输入【#开始聊天】，即可开始连续聊天，输入【#结束聊天】，即可结束聊天；\n" +
+                            "5.帮助:输入【帮助】，查看当前帮助信息；\n" +
                             "请注意，连续聊天功能会耗费大量的tokens，请节制使用。";
                 } else {
                     if (user_status != null && user_status.size() > 0 && user_status.get(user_id)){
-                        //30分钟到期
                         ArrayList<Message> messages;
-                        if (user_time.get(user_id).getTime() + EXPIRED_TIME > System.currentTimeMillis()){
+                        if (user_time.get(user_id).getTime() + EXPIRED_TIME < System.currentTimeMillis()){
+                            //30分钟已到，连续状态到期
                             user_status.remove(user_id);
                             user_time.remove(user_id);
                             user_messages.remove(user_id);
@@ -118,17 +122,23 @@ public class QqRobotServiceImpl implements QqRobotService {
                     }
                 }
                 //发送消息
-                sendPrivateMsg(aiMessage, user_id);
+                //sendPrivateMsg(aiMessage, user_id);
+                result.put("reply",aiMessage);
+                result.put("auto_escape",false);
             } catch (Exception e) {
                 log.error("QqRobotService出错:{}", e.getMessage());
                 e.printStackTrace();
                 sendPrivateMsg(e.getMessage(),user_id);
             }
         }else if ("request".equals(post_type) && "friend".equals(request_type)){
+            //自动通过好友请求
             String flag = jsonParam.getString("flag");
             try {
+                //result.put("approve",true);
                 agreeFriendRequest(flag,true,null);
-                sendPrivateMsg(user_id+"好友添加成功！","");
+                //休眠1s，好友通过后再发送消息
+                Thread.sleep(1000);
+                sendPrivateMsg("请发送【帮助】查看提示信息。",user_id);
             } catch (Exception e) {
                 log.error("QqRobotService出错:{}", e.getMessage());
                 e.printStackTrace();
@@ -136,6 +146,7 @@ public class QqRobotServiceImpl implements QqRobotService {
             }
         }
         log.debug("QqRobotServiceImpl.QqRobotEvenHandle结束");
+        return result;
     }
 
     /**
@@ -173,10 +184,10 @@ public class QqRobotServiceImpl implements QqRobotService {
         params.put("flag", flag);
         params.put("approve", approve);
         params.put("remark", remark);
-        log.info("私聊消息接口url:{}", url);
-        log.info("私聊消息接口参数:{}", params);
+        log.info("同意好友申请接口url:{}", url);
+        log.info("同意好友申请接口参数:{}", params);
         String response = HttpUtils.sendPost(url, null, HttpUtils.asUrlParams(params));
-        log.info("私聊消息接口返回:{}", response);
+        log.info("同意好友申请接口返回:{}", response);
         log.debug("QqRobotServiceImpl.agreeFriendRequest结束");
     }
 
